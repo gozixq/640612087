de5b34b6660ca5e0320816bac203bd68de17c5c6
gozixq <natsinee2545@gmail.com>
2022-10-06 15:59:12 +0700

    finished3

--- pages/api/summary.js
+++ pages/api/summary.js
@@ -1,9 +1,31 @@
 export default function summaryRoute(req, res) {
   if (req.method === "GET") {
+
     //check authentication
-    //return res.status(403).json({ ok: false, message: "Permission denied" });
+    const user = checkToken(req);
+
+    if (!user || !user.isAdmin){
+
+      return res.status(403).json({ ok: false, message: "Permission denied" });
+    }
+      
     //compute DB summary
+    const users = readUsersDB();
+    let User = 0;
+    let Admin = 0;
+    let Sum = 0;
+
+    users.forEach((x) => { User++;
+      if (x.isAdmin){
+        Admin++;
+        Sum = Sum + x.money;
+      }
+    });
+
+
     //return response
+    return res.json({ ok: true,User ,Admin ,Sum });
+  
   } else {
     return res.status(400).json({ ok: false, message: "Invalid HTTP Method" });
   }
--- pages/api/withdraw.js
+++ pages/api/withdraw.js
@@ -1,20 +1,40 @@
 export default function withdrawRoute(req, res) {
   if (req.method === "PUT") {
     //check authentication
-    //return res.status(403).json({ ok: false, message: "You do not have permission to withdraw" });
+    const user = checkToken(req);
+    if (!user || user.isAdmin){
 
+      return res.status(403).json({ ok: false, message: "You do not have permission to withdraw" });
+
+    }
+    
     const amount = req.body.amount;
     //validate body
     if (typeof amount !== "number")
       return res.status(400).json({ ok: false, message: "Invalid amount" });
 
     //check if amount < 1
-    // return res.status(400).json({ ok: false, message: "Amount must be greater than 0" });
+    if (amount < 0){
+
+      return res.status(400).json({ ok: false, message: "Amount must be greater than 0" });
+    }
+    
 
     //find and update money in DB (if user has enough money)
-    //return res.status(400).json({ ok: false, message: "You do not has enough money" });
+    const users = readUsersDB();
+    const Uindex = users.findIndex((x) => x.username === user.username);
+    
+    if (users[Uindex].money < amount){
+      return res.status(400).json({ ok: false, message: "You do not has enough money" });
+    }
+
+    users[Uindex].money -= amount;
+    writeUsersDB(users);
 
     //return response
+
+    return res.json({ ok: true, money: users[Uindex].money });
+    
   } else {
     return res.status(400).json({ ok: false, message: "Invalid HTTP Method" });
   }
